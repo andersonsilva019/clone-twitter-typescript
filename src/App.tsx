879f4f56b4620423e9530dc55e762460fbe444d4
Anderson0017 <andeerson0017@gmail.com>
2020-07-09 11:17:13 -0300

    create main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import GlobalStyles from './styles/global';
+import Layout from './Layout';
 
 const App: React.FC = () => {
   return (
     <>
-      <h1>Teste</h1>
+      <Layout />
       <GlobalStyles />
     </>
   );
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import PrfilePage from '../../components/ProfilePage';
+
+import {
+  Container,
+  Header,
+  BackIcon,
+  ProfileInfo,
+  BottunMenu,
+  HomeIcon,
+  SearchIcon,
+  BellIcon,
+  EmailIcon,
+} from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+        <ProfileInfo>
+          <strong>Anderson silva</strong>
+          <span>1512221 Tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      <ProfilePage />
+
+      <BottunMenu>
+        <HomeIcon className="active" />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottunMenu>
+    </Container>
+  );
+};
+
+export default Main;
